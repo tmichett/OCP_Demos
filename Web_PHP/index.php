5928e372a4e56901170446a90476c34eb3cfde50
Travis Michette <tmichett@redhat.com>
2021-09-10 16:56:03 -0400

    Modified PHP Code

---
+++ Web_PHP/index.php
@@ -0,0 +1,9 @@
+<html>
+ <head>
+  <title>PHP Sample Website</title>
+<?php echo "<p>Welcome to my website</p>" ?>
+<?php echo gethostname(); ?>
+<?php echo "I'm running PHP version:" phpinfo(); ?>
+
+ </body>
+</html>
