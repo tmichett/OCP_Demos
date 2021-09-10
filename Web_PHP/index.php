360a3ff3d98d7d5ae57dd6a57e51f0739b69923f
Travis Michette <tmichett@redhat.com>
2021-09-10 17:11:57 -0400

    Fixed PHP and tested

--- Web_PHP/index.php
+++ Web_PHP/index.php
@@ -2,8 +2,9 @@
  <head>
   <title>PHP Sample Website</title>
 <?php echo "<p>Welcome to my website</p>" ?>
+<?php echo "<p>My Hostname is:</p>" ?>
 <?php echo gethostname(); ?>
-
+<?php phpinfo(); ?>
 
  </body>
 </html>
