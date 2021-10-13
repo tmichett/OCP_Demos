5008ef1b9b632a89e3b5af208aad5f5b971959f7
Travis Michette <tmichett@redhat.com>
2021-10-13 16:07:22 -0400

    Update index.php

--- Web_PHP/index.php
+++ Web_PHP/index.php
@@ -1,7 +1,7 @@
 <html>
  <head>
   <title>PHP Sample Website</title>
-<?php echo "<p>Welcome to my website - Demo 2</p>" ?>
+<?php echo "<p>Welcome to my website - RHU S2I Demo</p>" ?>
 <?php echo "<p>My Hostname is:</p>" ?>
 <?php echo gethostname(); ?>
 <?php phpinfo(); ?>
