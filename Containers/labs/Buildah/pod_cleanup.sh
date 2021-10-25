b8b455acae8d70b8073098c3b07a3d615cea200a
Travis Michette <tmichett@redhat.com>
2021-10-25 15:19:43 -0400

    pod scripts

---
+++ Containers/labs/Buildah/pod_cleanup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+sudo podman rm -f db wp
+sudo podman pod rm -f wordpress_demo_pod
+sudo podman volume rm -a -f
+
+# This will display message when cleanup has completed.
+echo "The Pod Demo has completed and is cleaned up!!"
