2e4d7839a25cf9225c826ef09ea4c95df1c1817a
Travis Michette <tmichett@redhat.com>
2021-10-28 15:27:42 -0400

    Updated Chapter 4 Buildah information and created script

---
+++ Containers/labs/Buildah/rootless_buildah_demo.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+## Define Container Variable and Mountpoint Variable
+ctr=$(buildah from scratch)
+mnt=$(buildah mount $ctr)
+
+## Install Files
+yumdownloader --destdir=/tmp redhat-release-server
+rpm -ivh --root $mnt /tmp/redhat-release-8.4-0.6.el8.x86_64.rpm
+cp  /etc/yum.repos.d/rhel_dvd.repo $mnt/etc/yum.repos.d/
+yum -y install --installroot $mnt httpd
+echo "This is a custom webserver container for me" >> $mnt/var/www/html/index.html
+yum install --installroot $mnt httpd-manual
+
+
+## Configure Container Image
+buildah config --cmd "/usr/sbin/httpd -DFOREGROUND" $ctr
+buildah config --port 80/tcp $ctr
+buildah config --author "Travis Michette <tmichett@redhat.com>" $ctr
+
+## Cleanup Image
+yum clean all --installroot $mnt
+
+## Unmount and Commit Image 
+buildah unmount $ctr
+buildah commit $ctr myapp
