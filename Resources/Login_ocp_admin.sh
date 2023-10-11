197a6759b63d287a8501812ecdf1d11adc75a97f
Travis Michette <tmichett@redhat.com>
2023-10-11 14:54:48 -0400

    OCP Login Scripts Base OCP Courses

---
+++ Resources/Login_ocp_admin.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+#
+# Travis Michette <tmichett@redhat.com>
+#
+
+oc login -u admin -p redhatocp https://console-openshift-console.apps.ocp4.example.com
---
+++ Resources/Login_ocp_developer.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+#
+# Travis Michette <tmichett@redhat.com>
+#
+
+oc login -u developer -p developer https://console-openshift-console.apps.ocp4.example.com
